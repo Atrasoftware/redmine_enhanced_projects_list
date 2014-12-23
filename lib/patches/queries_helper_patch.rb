f356c7861fce0fa77bbebe7afad4725f0504d936
Bilel KEDIDI <bilel.kedidi@gmail.com>
2014-12-23 12:00:56 +0100

    fix bug

--- lib/patches/queries_helper_patch.rb
+++ lib/patches/queries_helper_patch.rb
@@ -13,13 +13,17 @@ module  Patches
     end
   end
       module ClassMethods
+        def get_plugin_redmine_enhanced
+          settings = Setting.send "plugin_redmine_enhanced_projects_list"
+          CustomField.where(:type=> "ProjectCustomField").order("name ASC").select{|col| settings[col.name.to_sym] }.map(&:name)
+        end
       end
 
       module InstanceMethods
         def query_available_inline_columns_options_with_add(query)
           queries = (query.available_inline_columns - query.columns).reject(&:frozen?).collect {|column| [column.caption, column.name]}
           sessions = session[:query][:column_names]
-            cfs = get_plugin_redmine_enhanced
+            cfs = QueriesHelper.get_plugin_redmine_enhanced
             cfs.each do |cf|
               sessions ? (queries<< [cf, cf] unless sessions.include?(cf.to_sym)) : queries<< [cf, cf]
             end
@@ -30,7 +34,7 @@ module  Patches
           queries= (query.inline_columns & query.available_inline_columns).reject(&:frozen?).collect {|column| [column.caption, column.name]}
           sessions = session[:query][:column_names]
           if sessions
-            cfs = get_plugin_redmine_enhanced
+            cfs = QueriesHelper.get_plugin_redmine_enhanced
             cfs.each do |cf|
               queries<< [cf, cf] if sessions.include?(cf.to_sym)
             end
@@ -38,10 +42,5 @@ module  Patches
           queries
         end
 
-        private
-        def get_plugin_redmine_enhanced
-          settings = Setting.send "plugin_redmine_enhanced_projects_list"
-          CustomField.where(:type=> "ProjectCustomField").order("name ASC").select{|col| settings[col.name.to_sym] }.map(&:name)
-        end
       end
     end
