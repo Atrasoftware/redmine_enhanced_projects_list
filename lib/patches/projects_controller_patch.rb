aad21342a6cf0a261882d3292c8485de47f45b4f
Bilel KEDIDI <bilel.kedidi@gmail.com>
2015-01-05 23:43:28 +0100

    fix bug

--- lib/patches/projects_controller_patch.rb
+++ lib/patches/projects_controller_patch.rb
@@ -40,11 +40,15 @@ module  Patches
           @projects =  scope.visible.where("parent_id is null").order(order).offset(@offset).limit(@limit)
         }
         format.api  {
-
+          @settings = Setting.send "plugin_redmine_enhanced_projects_list"
+          order = 'identifier'
+          if @settings[:sorting_projects_order] == 'true'
+            order = 'identifier DESC'
+          end
           @offset, @limit = api_offset_and_limit
           @project_count = Project.visible.count
-         # @projects = Project.visible.offset(@offset).limit(@limit).order('lft').all
-          @projects = Project.get_all_projects(Project.visible.all, true)[@offset..@limit]
+          @projects = Project.visible.offset(@offset).limit(@limit).order(order).all
+          #@projects = Project.get_all_projects(Project.visible.all, order == 'identifier DESC')[@offset..@limit]
         }
         format.atom {
           projects = Project.visible.order('created_on DESC').limit(Setting.feeds_limit.to_i).all
