b855ef471c14e355f45b5bd177cd8c112b97ae27
Bilel KEDIDI <bilel.kedidi@gmail.com>
2015-01-06 00:09:40 +0100

    fix bug

--- lib/patches/projects_controller_patch.rb
+++ lib/patches/projects_controller_patch.rb
@@ -42,7 +42,7 @@ module  Patches
         format.api  {
           @offset, @limit = api_offset_and_limit
           @project_count = Project.visible.count
-          binding.pry
+         
           @projects = Project.visible.offset(@offset).limit(@limit).order('identifier').all
           #@projects = Project.get_all_projects(Project.visible.all, order == 'identifier DESC')[@offset..@limit]
         }
