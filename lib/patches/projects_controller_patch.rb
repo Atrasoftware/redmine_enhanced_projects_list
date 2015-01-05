f5309172a066c7c7b629bdd5c43ff5a96b096ef2
Bilel KEDIDI <bilel.kedidi@gmail.com>
2015-01-05 23:02:31 +0100

    fix bug

--- lib/patches/projects_controller_patch.rb
+++ lib/patches/projects_controller_patch.rb
@@ -20,23 +20,91 @@ module  Patches
   module InstanceMethods
 
     def index_with_filter_project
+      @settings = Setting.send "plugin_redmine_enhanced_projects_list"
+      order = 'identifier'
+      if @settings[:sorting_projects_order] == 'true'
+        order = 'identifier DESC'
+      end
       respond_to do |format|
         format.html {
           scope = Project
           unless params[:closed]
             scope = scope.active
           end
-          @projects = scope.visible.order('lft').all
+          #change the limit HERE
+          # PS: LIMIT are set for project root ( parent id = null)
+          @limit  = per_page_option
+          @projects_count  = scope.visible.where("parent_id is null").size
+          @projects_pages = Redmine::Pagination::Paginator.new @projects_count, @limit, params['page']
+          @offset ||=@projects_pages.offset
+          @projects =  scope.visible.where("parent_id is null").order(order).offset(@offset).limit(@limit)
         }
         format.api  {
           @offset, @limit = api_offset_and_limit
           @project_count = Project.visible.count
-          @projects = Project.visible.offset(@offset).limit(@limit).order('lft').all
+         # @projects = Project.visible.offset(@offset).limit(1).order('lft').all
+          @projects = Project.get_all_projects(Project.visible, order == 'identifier DESC')[@offset..@limit]
         }
         format.atom {
           projects = Project.visible.order('created_on DESC').limit(Setting.feeds_limit.to_i).all
           render_feed(projects, :title => "#{Setting.app_title}: #{l(:label_project_latest)}")
         }
+        format.pdf{
+          scope = Project
+          unless params[:closed]
+            scope = scope.active
+          end
+          @settings = Setting.send "plugin_redmine_enhanced_projects_list"
+          if params[:project_search]
+            scope= scope.visible.where("name like ? or identifier like ? ","%#{params[:project_search]}%","%#{params[:project_search]}%")
+            val = CustomValue.where(:customized_type=> 'Project').where("value like ?", "%#{params[:project_search]}%" )
+            val.each do |pr|
+              scope<< pr.customized unless scope.map(&:identifier).include? pr.customized.identifier
+            end
+          else
+            order = 'identifier'
+            if @settings[:sorting_projects_order] == 'true'
+              order = 'identifier DESC'
+            end
+            @limit  = per_page_option
+            @projects_count  = scope.visible.where("parent_id is null").size
+            @projects_pages = Redmine::Pagination::Paginator.new @projects_count, @limit, params['page']
+            @offset ||=@projects_pages.offset
+            scope= scope.visible.where("parent_id is null").order(order).offset(@offset).limit(@limit)
+          end
+          @projects = scope
+              settings= Array.new
+          settings << l("field_identifier".to_sym)
+          Project.column_names.select{|col| @settings[col.to_sym] and col!= "identifier" }.each do |column|
+            settings<< l("field_#{column}".to_sym)
+          end
+          CustomField.where(:type=> "ProjectCustomField").order("name ASC").select{|col| @settings[col.name.to_sym] }.each do |cf|
+            settings<< cf.name
+          end
+          order_desc = false
+          if @settings[:sorting_projects_order] == 'true'
+            order_desc = true
+          end
+          @settings= @settings.except(:sorting_projects_order)
+        send_data(ProjectsHelper.to_pdf(@projects,@settings,settings,'FR',order_desc), :type => 'application/pdf', :filename => 'projects.pdf')
+        }
+=begin
+        format.js{
+          scope = Project
+          unless params[:closed]
+            scope = scope.active
+          end
+          @settings = Setting.send "plugin_redmine_enhanced_projects_list"
+          if params[:project_search]
+            scope= scope.visible.where("name like ? or identifier like ? ","%#{params[:project_search]}%","%#{params[:project_search]}%")
+            val = CustomValue.where(:customized_type=> 'Project').where("value like ?", "%#{params[:project_search]}%" )
+            val.each do |pr|
+              scope<< pr.customized unless scope.map(&:identifier).include? pr.customized.identifier
+            end
+          end
+          @projects = scope
+        }
+=end
       end
     end
 
