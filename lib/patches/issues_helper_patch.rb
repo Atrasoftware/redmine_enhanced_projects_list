4aa6c1ae2749b8165619b4bf451549357578893c
Bilel KEDIDI <bilel.kedidi@gmail.com>
2015-01-18 10:41:45 +0100

    add_issues_tree

--- lib/patches/issues_helper_patch.rb
+++ lib/patches/issues_helper_patch.rb
@@ -8,13 +8,24 @@ module  Patches
       base.send(:include, InstanceMethods)
       base.class_eval do
         alias_method_chain :issue_list, :order
+
+        def get_issue_tree(tree, issue, issues, &block)
+          tree<< issue if !tree.include? issue and issues.include? issue
+          children = issue.children
+          children.each do |child|
+            tree = get_issue_tree(tree, child, issues, &block)
+          end
+          tree
+        end
       end
 
       end
 
   end
+
   module ClassMethods
   end
+
   module InstanceMethods
     def issue_list_with_order(issues, &block)
       if params[:group_by] == "project"
@@ -25,8 +36,24 @@ module  Patches
         end
         issues =  new_issues.flatten
       end
-      issue_list_without_order(issues, &block)
-    end
+      #issue_list_without_order(issues, &block)
+      tree= []
+      issues.each do |issue|
+        tree = get_issue_tree(tree, issue, issues, &block)
+      end
+      issues = tree
+
+      ancestors = []
+      issues.each do |issue|
+        while ancestors.any? && !issue.is_descendant_of?(ancestors.last)
+          ancestors.pop
+        end
+        yield issue, ancestors.size
+        ancestors << issue unless issue.leaf?
+      end
+
+    end # end method issue list
+
   end
 
 end
