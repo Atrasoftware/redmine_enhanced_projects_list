35a8da1bdcd46a36eca08c4b5df7f6d84b214b17
Bilel KEDIDI <bilel.kedidi@gmail.com>
2015-02-09 08:30:45 +0100

    updates

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ Redmine::Plugin.register :redmine_enhanced_projects_list do
   name 'Redmine Enhanced Projects List plugin'
   author 'Bilel KEDIDI'
   description 'This is a plugin for Redmine'
-  version '0.0.1'
+  version '0.1.0'
   url 'https://github.com/Atrasoftware/redmine-enhanced-projects-list'
 
   permission :copy, :projects => :copy
